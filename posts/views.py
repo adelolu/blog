2717ed3ec48ab702a3181c5b546fc33add45c386
temiloluwa oyinloye <adelolu6@gmail.com>
2024-04-27 13:11:49 +0100

    end of blog tutorial with django

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def index (req):
-    return render(req, 'index.html')
+    posts = Post.objects.all()
+    return render(req, 'index.html', {'posts': posts})
+
+def post (req, pk):
+    posts = Post.objects.get(id=pk)
+    return render(req, 'posts.html', {'posts': posts})
